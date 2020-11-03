58bfbaa70000985bf153ab44465d03ca71d85d9e
jake mcgee <mightymini05@gmail.com>
2020-11-03 12:45:17 -0700

    latest js commits for starships

---
+++ starships/main.js
@@ -0,0 +1,9 @@
+import { starships } from '../data/starships.js'
+
+const nav = document.querySelector('.nav')
+const navlist = document.querySelector('.navlist')
+const shipView = document.querySelector('.main')
+
+function populateNav(starships) {
+    
+}
--- starwars people/main.js
+++ starwars people/main.js
@@ -2,7 +2,7 @@ import { people } from '../data/people.js'
 
 const mainContent = document.querySelector('main')
 
-const mainheader = document.createElement('header')
+const mainHeader = document.createElement('header')
 document.body.appendChild(mainHeader)
 
 const maleButton = doument.createElement('button')
@@ -20,10 +20,14 @@ mainHeader.appendChild(otherButton)
 const maleCharacters = people.filter(person => person.gender === 'male')
 console.log(maleCharacters)
 
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(person => person.gender === 'other')
+console.log(otherCharacters)
+
 maleButton.addEventListener('click', (event) => {
-    const charFirgure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    const charCaption = document.createElement('figcaption')
+    
 
 charFirgure.appendChild(charImg)
 charFirgure.appendChild(charCaption)
@@ -41,7 +45,7 @@ function populateDOM(charcters) {
         const charCaption = document.createElement('figcaption')
 
         charFigure.appendChild(charImg)
-        
+
     }
 }
 
