17cda590aaf72f2e2db57a176dcb8e16a40e7de6
jake mcgee <mightymini05@gmail.com>
2020-11-10 23:35:55 -0700

    final edits on js and css

--- starships/main.js
+++ starships/main.js
@@ -1,17 +1,29 @@
 import { starships } from '../data/starships.js'
-import { removeChildren, getLastNumber } from '../utilities/index.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navlist = document.querySelector('.navlist')
 const shipView = document.querySelector('.main')
 
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
 function populateNav(starships) {
-    starships.forEach(seatship => {
+    starships.forEach(starship => {
         let anchorWrap = document.createElement('a')
         anchorWrap.href = '#'
-        anchorWrap.addEventListener('click', event =>{
-            var shipname = event.target.textContent
-            const foundShip = starships.find(ship = ship.name === shipName)
+        anchorWrap.addEventListener('click', event => {
+            var shipName = event.target.textContent
+            const foundShip = starships.find(ship => ship.name === shipName)
             populateShipView(foundShip)
         })
 
@@ -25,7 +37,15 @@ function populateNav(starships) {
 }
 
 function populateShipView(shipData) {
-
+    removeChildren(shipView)
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        dialog.classList.toggle("is-active")
+    }) 
+    shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
--- starwars-people/main.js
+++ starwars-people/main.js
@@ -1,69 +1,63 @@
 import { people } from '../data/people.js'
 
-const mainContent = document.querySelector('main')
+const mainContent = document.querySelector('#main')
 
-populateDOM(people)
-
-const mainHeader = document.createElement('header')
+const mainHeader = document.querySelector('header')
 mainHeader.className = 'mainHeader'
-document.body.appendChild(mainHeader)
+document.body.appendChild(mainHeader, mainContent)
 
-const maleButton = doument.createElement('button')
-maleButtonButton.textContent = 'Male Characters'
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
 mainHeader.appendChild(maleButton)
 
-const femaleButton = doument.createElement('button')
-femaleButtonButton.textContent = 'Female Characters'
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
 mainHeader.appendChild(femaleButton)
 
-const otherButton = doument.createElement('button')
-otherButtonButton.textContent = 'Other Characters'
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
 const maleCharacters = people.filter(person => person.gender === 'male')
 
 const femaleCharacters = people.filter(person => person.gender === 'female')
 
-const otherCharacters = people.filter(person => {
-    if (person.gender === 'n/a' ||
-    person.gender === 'none' ||
-    person.gender === 'hermaphrodite') {
-        return person
-    }
-})
+const otherCharacters = people.filter(person => person.gender === 'n/a')
+
 
 maleButton.addEventListener('click', (event) => {
-    populateDOM(maleCharacters)
+   populateDOM(maleCharacters)
 })
 
-femaleButton.addEventListener('click', (event) => populateDOM(femaleCharacters))
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 
-otherButton.addEventListener('click', ()=> populateDOM(otherCharacters))
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
 
 function populateDOM(characters) {
     removeChildren(mainContent)
-    characters.forEach(element=> {
+    characters.forEach(element => {
         const charFigure = document.createElement('figure')
         const charImg = document.createElement('img')
         let charNum = getLastNumber(element.url)
-        charImg.src = 'https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg'
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
         charImg.addEventListener('error', () => charImg.hidden = true)
-        const charCaption = document.createElement('figcaption') 
+        const charCaption = document.createElement('figcaption')
         charCaption.textContent = element.name
 
         charFigure.appendChild(charImg)
-        charFigure. appendChild(charCaption)
+        charFigure.appendChild(charCaption)
 
         mainContent.appendChild(charFigure)
     })
 }
 
-/* let theURL = "https:// swapi.co/api/people/2/"
-let theURL2 = "https:// swapi.co/api/people/14/" */
 
+/* let theURL = "https://swapi.co/api/people/2/"
+let theURL2 = "https://swapi.co/api/people/14/"
+ */
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
-    let start = end - 2 
+    let start = end - 2
     if (url.charAt(start) === '/') {
         start++
     }
@@ -72,7 +66,10 @@ function getLastNumber(url) {
 
 function removeChildren(container) {
     while (container.firstChild) {
-        container.removeChild(container.firstChild);
+        element.removeChild(container.firstChild);
     }
 }
+
+
+//https://starwars-visualguide.com/assets/img/characters/1.jpg
 
