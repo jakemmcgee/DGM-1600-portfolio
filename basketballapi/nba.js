483073e136051f7466a9ec8f5010604faa6939a3
jake mcgee <mightymini05@gmail.com>
2020-12-16 11:14:44 -0700

    fetching data for api folder

--- basketballapi/nba.js
+++ basketballapi/nba.js
@@ -1,9 +1,42 @@
-import { teams } from '../data/'
+import { confName } from '../data/nbaapi.js'
 
 const teamGrid = document.querySelector('#teamGrid')
-const teamButton = document.querySelector('#teamButton')
+const eastButton = document.querySelector('#eastButton')
+const westButton = document.querySelector('#westButton')
 
-teamButton.addEventListener('click', () => {
+eastButton.addEventListener('click', () => {
     colorSort()
 })
 
+westButton.addEventListener('click', () => {
+    colorSort()
+})
+
+
+fetch("https://api-nba-v1.p.rapidapi.com/teams/confName/west", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5dcc6a71admsh224769d2ea90948p11f213jsn32545ca82f68",
+		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+fetch("https://api-nba-v1.p.rapidapi.com/teams/confName/east", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5dcc6a71admsh224769d2ea90948p11f213jsn32545ca82f68",
+		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
---
+++ data/nbaapi.js
@@ -0,0 +1,90 @@
+{
+"api" ; {
+"status" ; 200
+"message" ; "GET teams/confName/east"
+"results" ; 20
+"filters" ; [
+0 , "teamId",
+1 , "league",
+2 , "city",
+3 , "shortName",
+4 , "nickName",
+5 , "confName",
+6 , "divName",
+]
+"teams";[
+0 , {
+"city" : "Atlanta",
+"fullName" : "Atlanta Hawks",
+"teamId" : "1",
+"nickname" : "Hawks",
+"logo" : "https://upload.wikimedia.org/wikipedia/fr/e/ee/Hawks_2016.png",
+"shortName" : "ATL",
+"allStar" : "0",
+"nbaFranchise" : "1",
+"leagues" : {
+"standard" : {
+"confName" : "East",
+"divName" : "Southeast"}
+}
+}
+
+1 , {
+"city" : "Boston",
+"fullName" : "Boston Celtics",
+"teamId" : "2",
+"nickname" : "Celtics",
+"logo" : "https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Celtics_de_Boston_logo.svg/1024px-Celtics_de_Boston_logo.svg.png",
+"shortName" : "BOS",
+"allStar" : "0",
+"nbaFranchise" : "1",
+"leagues" : {
+"standard" : {
+"confName" : "East",
+"divName" : "Atlantic"}
+}
+}
+
+2:{...}9 items
+3:{...}9 items
+4:{...}9 items
+5:{...}9 items
+6:{...}9 items
+7:{...}9 items
+8:{...}9 items
+9:{...}9 items
+10:{...}9 items
+11:{...}9 items
+12:{...}9 items
+13:{...}9 items
+14:{...}9 items
+15:{...}9 items
+16:{...}9 items
+17:{...}9 items
+18:{...}9 items
+19:{...}9 items
+]
+}
+}
+}9 items
+2;{...}9 items
+3;{...}9 items
+4;{...}9 items
+5;{...}9 items
+6;{...}9 items
+7;{...}9 items
+8;{...}9 items
+9;{...}9 items
+10;{...}9 items
+11;{...}9 items
+12;{...}9 items
+13;{...}9 items
+14;{...}9 items
+15;{...}9 items
+16;{...}9 items
+17;{...}9 items
+18;{...}9 items
+19;{...}9 items
+]
+}
+}
